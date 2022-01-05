a643e2a0d35cca7526884b0a51efcdd369894adb
Lee-TB <86998194+Lee-TB@users.noreply.github.com>
2022-01-05 07:28:54 +0700

    JS Modules

---
+++ Modules/app.js
@@ -0,0 +1,6 @@
+import {logger2} from "./index.js";
+import logger from "./index.js"
+import * as constants from './constants.js'
+
+logger('this is logger', constants.TYPE_ERROR)
+logger2('Hello World')
